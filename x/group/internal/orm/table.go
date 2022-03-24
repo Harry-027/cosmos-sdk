516f83006926681ef862dda21611b5237c280f77
Amaury M <1293565+amaurym@users.noreply.github.com>
2022-03-24 11:22:57 +0100

    Remove commented code

--- x/group/internal/orm/table.go
+++ x/group/internal/orm/table.go
@@ -125,11 +125,11 @@ func (a table) Set(store sdk.KVStore, rowID RowID, newValue codec.ProtoMarshaler
 
 	fmt.Println("table set L", rowID)
 	pStore.Set(rowID, newValueEncoded)
-	// for i, itc := range a.afterSet {
-	// 	if err := itc(store, rowID, newValue, oldValue); err != nil {
-	// 		return sdkerrors.Wrapf(err, "interceptor %d failed", i)
-	// 	}
-	// }
+	for i, itc := range a.afterSet {
+		if err := itc(store, rowID, newValue, oldValue); err != nil {
+			return sdkerrors.Wrapf(err, "interceptor %d failed", i)
+		}
+	}
 	fmt.Println("table set M")
 	return nil
 }
